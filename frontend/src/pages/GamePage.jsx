646e60d5231a1b3970e1bb832ca4698f2a995a71
hssmith27 <hssmith2704@gmail.com>
2026-01-05 15:06:07 -0500

    Functioning Dealing

--- frontend/src/pages/GamePage.jsx
+++ frontend/src/pages/GamePage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Card from '../components/Card.jsx'
 import '../pages_styles/GamePage.css'
 
@@ -8,14 +8,55 @@ function GamePage() {
     const [handsCorrect, setHandsCorrect] = useState(0)
 
     // Store player and dealer cards
-    const [playerCards, setPlayerCards] = useState(["AH", "0C"])
-    const [dealerCard, setDealerCard] = useState("AS")
+    const [playerCards, setPlayerCards] = useState([])
+    const [dealerCard, setDealerCard] = useState("")
 
     // Hand Info
-    const [trueCount, setTrueCount] = useState(1)
+    const [trueCount, setTrueCount] = useState(0)
 
     // AI Policy
     const [modelPolicy, setModelPolicy] = useState("")
+
+    // Deck management
+    const [deckID, setDeckID] = useState("")
+    const [numCards, setNumCards] = useState(0)
+
+    // Load shoe when loading the page
+    useEffect(() => {
+        loadShoe();
+    }, [])
+
+    // Loads in the shoe from an API
+    async function loadShoe() {
+        const response = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
+        const data = await response.json();
+        setDeckID(data['deck_id']);
+        setNumCards(data['remaining']);
+    }
+
+    // Reset shoe
+    async function resetShoe() {
+        const response = await fetch('https://deckofcardsapi.com/api/deck/' + deckID + '/shuffle/');
+        const data = await response.json();
+        setNumCards(data['remaining']);
+    }
+
+    // Deal cards to player and dealer
+    async function dealCards() {
+        const response = await fetch('https://deckofcardsapi.com/api/deck/' + deckID + '/draw/?count=3');
+        const data = await response.json();
+        setNumCards(data['remaining']);
+
+        // Set cards
+        const cards = data['cards'].map(card => card.code);
+        setPlayerCards(cards.slice(0, 2))
+        setDealerCard(cards[2])
+
+        // Reset shoe if less than deck remaining
+        if (data['remaining'] < 52) {
+            resetShoe();
+        }
+    }
 
     // Fetches the optimal policy from the backend
     async function fetchOptimal() {
@@ -25,7 +66,7 @@ function GamePage() {
             body: JSON.stringify({player_cards: playerCards, dealer_card: dealerCard, true_count: trueCount})
         });
         const data = await response.json();
-        setModelPolicy(data['policy'])
+        setModelPolicy(data['policy']);
     }
 
     return (
@@ -44,10 +85,13 @@ function GamePage() {
                 </div>
                 <div className="game-actions" >
                     <button onClick={fetchOptimal}>{modelPolicy}</button>
+                    <button onClick={loadShoe}>Button</button>
+                    <button onClick={resetShoe}>Button</button>
+                    <button onClick={dealCards}>Deal</button>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default GamePage
